eaa4ed213cb1065fa8477c2c5c69db84cf8b9ac6
Pramod JM <pramodjm4@gmail.com>
2025-01-20 09:48:08 +0530

    await in mongodb

--- config/connect.db.js
+++ config/connect.db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 export const connectMongooseDB = async () => {
   try {
-    const dbData = mongoose.connect(process.env.MONGO_CLOUD);
+    const dbData =await mongoose.connect(process.env.MONGO_CLOUD);
     console.log("MONGO Cloud DB connected 👍!!!");
   } catch (err) {
     console.log("Error in DB😒", err);
