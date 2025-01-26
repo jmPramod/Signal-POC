906ddcf08821711a09f8378526979024f829b6be
J M pramod <pramod@ctinfopro.com>
2025-01-26 09:48:29 +0530

    mobile responsive

--- src/components/Navbar/DropDownMenu.jsx
+++ src/components/Navbar/DropDownMenu.jsx
@@ -1,8 +1,8 @@
-"use client"
+'use client';
 
-import * as React from "react"
+import * as React from 'react';
 
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -10,28 +10,27 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useNavigate, useNavigationType } from "react-router-dom"
-import { GlobalContext } from "@/context/GlobalContext"
+} from '@/components/ui/dropdown-menu';
+import { useNavigate, useNavigationType } from 'react-router-dom';
+import { GlobalContext } from '@/context/GlobalContext';
 
 export function DropdownMenuCheckboxes() {
-     const { user, setUser } = React.useContext(GlobalContext);
-     
+  const { user, setUser } = React.useContext(GlobalContext);
+
   const navigate = useNavigate();
-  const [showStatusBar, setShowStatusBar] = React.useState(true)
-  const [showActivityBar, setShowActivityBar] = React.useState(false)
-  const [showPanel, setShowPanel] = React.useState(false)
+  const [showStatusBar, setShowStatusBar] = React.useState(true);
+  const [showActivityBar, setShowActivityBar] = React.useState(false);
+  const [showPanel, setShowPanel] = React.useState(false);
   const handleLogin = async () => {
     if (user) {
-      localStorage.removeItem("User");
-      console.log("logout");
+      localStorage.removeItem('User');
+      console.log('logout');
       setUser(null);
-      navigate("/login");
-      
-      console.log("login redir");
+      navigate('/login');
+
+      console.log('login redir');
     } else {
-      navigate("/login");
-      
+      navigate('/login');
     }
   };
   return (
@@ -39,26 +38,26 @@ export function DropdownMenuCheckboxes() {
     //   <DropdownMenuTrigger asChild>
     //     <Button variant="outline">Open</Button>
     //   </DropdownMenuTrigger>
-      <DropdownMenuContent className="p-3 text-center flex flex-col items-center justify-center">
-        {/* <DropdownMenuLabel>Appearance</DropdownMenuLabel> */}
-        <DropdownMenuSeparator />
-        <DropdownMenuCheckboxItem
-       onClick={() => navigate("/profile")}
-       className="w-full text-1xl border hover:bg-black cursor-pointer hover:text-white p-2 m-1 text-center flex items-center justify-center"
+    <DropdownMenuContent className="p-3 text-center flex flex-col items-center justify-center">
+      {/* <DropdownMenuLabel>Appearance</DropdownMenuLabel> */}
+      <DropdownMenuSeparator />
+      <DropdownMenuCheckboxItem
+        onClick={() => navigate('/profile')}
+        className="w-full text-1xl border hover:bg-black cursor-pointer hover:text-white p-2 m-1 text-center flex items-center justify-center"
         //   checked={showStatusBar}
         //   onCheckedChange={setShowStatusBar}
-        >
-       Profile
-        </DropdownMenuCheckboxItem>
-                  <DropdownMenuCheckboxItem
-          checked={showPanel}
-          onClick={() => handleLogin()}
-          className="w-full text-1xl border hover:bg-black cursor-pointer hover:text-white p-2 m-1 text-center flex items-center justify-center"
-          //   onCheckedChange={setShowPanel}
-        >
-Logout
-        </DropdownMenuCheckboxItem>
-      </DropdownMenuContent>
+      >
+        Profile
+      </DropdownMenuCheckboxItem>
+      <DropdownMenuCheckboxItem
+        checked={showPanel}
+        onClick={() => handleLogin()}
+        className="w-full text-1xl border hover:bg-black cursor-pointer hover:text-white p-2 m-1 text-center flex items-center justify-center"
+        //   onCheckedChange={setShowPanel}
+      >
+        Logout
+      </DropdownMenuCheckboxItem>
+    </DropdownMenuContent>
     // </DropdownMenu>
-  )
+  );
 }
--- src/components/Navbar/MobileNav.jsx
+++ src/components/Navbar/MobileNav.jsx
@@ -4,53 +4,86 @@ import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import ListItem from '@mui/material/ListItem';
 import MenuIcon from '@mui/icons-material/Menu';
-import { styles } from "./styles";
+import { styles } from './styles';
 import { GlobalContext } from '@/context/GlobalContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function MobileNav() {
   const [isOpen, setIsOpen] = React.useState(false);
-const {user,setUser}=React.useContext(GlobalContext)
-  
-  
+  const { user, setUser } = React.useContext(GlobalContext);
+
+  const navigate = useNavigate();
+
   const toggleDrawer = (open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+    if (
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
       return;
     }
     setIsOpen(open);
   };
+  const handleLogin = async () => {
+    if (user) {
+      localStorage.removeItem('User');
+      console.log('logout');
+      setUser(null);
+      navigate('/login');
 
+      console.log('login redir');
+    } else {
+      navigate('/login');
+    }
+  };
   return (
     <div>
-      <Button onClick={toggleDrawer(true)} sx={{color:"white"}}>
-        <MenuIcon  color='white'/>
+      <Button onClick={toggleDrawer(true)} sx={{ color: 'white' }}>
+        <MenuIcon color="white" />
       </Button>
       <Drawer
         anchor="right"
         open={isOpen}
         onClose={toggleDrawer(false)}
         PaperProps={{
-          sx: { width: "50%" }, // Set the drawer width to 50% of the viewport
+          sx: { width: '50%' }, // Set the drawer width to 50% of the viewport
         }}
       >
         <Box
           sx={{
-            width: "100%", // Ensure the content fills the drawer width
-            padding: 2,    // Add some padding for spacing
+            width: '100%', // Ensure the content fills the drawer width
+            padding: 2, // Add some padding for spacing
           }}
           role="presentation"
           onClick={toggleDrawer(false)}
           onKeyDown={toggleDrawer(false)}
         >
           <styles.menuWrapperMob>
-           {user&& <ListItem>
-              <styles.menuButton variant="contained">Home</styles.menuButton>
-            </ListItem>}
-            <ListItem>
-              <styles.menuButton variant="contained">{user?"Login":"Logout"}</styles.menuButton>
+            {user && (
+              <ListItem>
+                <styles.menuButton
+                  variant="contained"
+                  onClick={() => navigate('/home')}
+                >
+                  Home
+                </styles.menuButton>
+              </ListItem>
+            )}
+            {user && (
+              <ListItem>
+                <styles.menuButton
+                  variant="contained"
+                  onClick={() => navigate('/profile')}
+                >
+                  Profile
+                </styles.menuButton>
+              </ListItem>
+            )}
+            <ListItem onClick={() => handleLogin()}>
+              <styles.menuButton variant="contained">
+                {!user ? 'Login' : 'Logout'}
+              </styles.menuButton>
             </ListItem>
-       {user&&     <ListItem>
-              <styles.menuButton variant="contained">Signal</styles.menuButton>
-            </ListItem>}
+            {/* */}
           </styles.menuWrapperMob>
         </Box>
       </Drawer>
